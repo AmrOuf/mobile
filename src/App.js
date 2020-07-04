3797b954652f08312b562c24830c21cc7d6e437a
Amr <amr.ouf96@gmail.com>
2020-07-04 11:40:31 +0200

    Added API call and application state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import Homepage from './pages/Homepage';
 import MobileDetails from './pages/MobileDetails';
 import Comparison from './pages/Comparison';
+import MobileCard from './components/MobileCard';
 
 function App() {
+  const [mobiles, setMobiles] = useState([]);
+  const [currentMobile, setCurrentMobile] = useState({});
+  const [loadingMode, setLoadingMode] = useState('manual');
+  const [comparedMobiles, setComparedMobiles] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const { data } = await axios.get(
+        'https://fonoapi.freshpixl.com/v1/getlatest',
+        {
+          params: {
+            token: '09fd60d4619c13d508190787214e4aea7227aa04c076ad73',
+            brand: 'samsung',
+          },
+        }
+      );
+      setMobiles(data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Fragment>
       <BrowserRouter>
         <Switch>
-          <Route path="/mobile-details" exact component={MobileDetails} />
-          <Route path="/comparison" exact component={Comparison} />
-          <Route path="/" exact component={Homepage} />
+          <Route
+            path="/mobile-details"
+            exact
+            render={() => <MobileDetails mobile={currentMobile} />}
+          />
+          <Route
+            path="/comparison"
+            exact
+            render={() => <Comparison comparedMobiles={comparedMobiles} />}
+          />
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <Homepage
+                mobiles={mobiles}
+                loadingMode={loadingMode}
+                setCurrentMobile={setCurrentMobile}
+                setComparedMobiles={setComparedMobiles}
+              />
+            )}
+          />
         </Switch>
       </BrowserRouter>
     </Fragment>

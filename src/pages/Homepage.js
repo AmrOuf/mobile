07077325dbb90d2c9c350a834ce2bfe429ce6aca
Amr <amr.ouf96@gmail.com>
2020-07-04 14:59:05 +0200

    Integrated the three tabs in Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,25 +1,79 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import Container from '@material-ui/core/Container';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import MobileCard from '../components/MobileCard';
 
-const Homepage = () => {
+import CompareTab from '../components/CompareTab';
+import ExploreTab from '../components/ExploreTab';
+import SettingsTab from '../components/SettingsTab';
+
+const Homepage = ({
+  mobiles,
+  loadingMode,
+  setCurrentMobile,
+  setComparedMobiles,
+  setLoadingMode,
+}) => {
   const [value, setValue] = useState(0);
   const [content, setContent] = useState(null);
 
+  useEffect(() => {
+    setContent(
+      <ExploreTab
+        mobiles={mobiles}
+        loadingMode={loadingMode}
+        setCurrentMobile={setCurrentMobile}
+      ></ExploreTab>
+    );
+  }, [mobiles]);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
-    setContent(newValue);
+
+    switch (newValue) {
+      case 0:
+        setContent(
+          <ExploreTab
+            mobiles={mobiles}
+            loadingMode={loadingMode}
+            setCurrentMobile={setCurrentMobile}
+          ></ExploreTab>
+        );
+        break;
+      case 1:
+        setContent(
+          <CompareTab
+            mobiles={mobiles}
+            setComparedMobiles={setComparedMobiles}
+          ></CompareTab>
+        );
+        break;
+      case 2:
+        setContent(
+          <SettingsTab
+            loadingMode={loadingMode}
+            setLoadingMode={setLoadingMode}
+          ></SettingsTab>
+        );
+        break;
+
+      default:
+        setContent(
+          <ExploreTab
+            mobiles={mobiles}
+            loadingMode={loadingMode}
+            setCurrentMobile={setCurrentMobile}
+          ></ExploreTab>
+        );
+    }
   };
 
   return (
     <Fragment>
       <Container>
-        <Paper gutterBottom>
+        <Paper>
           <Tabs
             value={value}
             onChange={handleChange}
@@ -32,8 +86,7 @@ const Homepage = () => {
             <Tab label="Settings" />
           </Tabs>
         </Paper>
-        <MobileCard></MobileCard>
-        <MobileCard></MobileCard>
+        {content}
       </Container>
     </Fragment>
   );
